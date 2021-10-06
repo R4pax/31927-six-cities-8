d5b50ca6e45cd7809fc5cb310fad9c3240d54380
Rapaxalar <72609217+Rapaxalar@users.noreply.github.com>
2021-10-06 22:27:38 +0200

    module3 task1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,40 @@
 import MainScreen from '../main-screen/main-screen';
 
 import { placeCards } from '../../types/PlaceCards';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Page404 from '../page-404/page-404';
+import LoginScreen from '../login-screen/login-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import PropertyScreen from '../property-screen/property-screen';
+import PrivateRoute from '../../hocs/private-route';
 
-function App({places} : {places: placeCards}): JSX.Element {
-  return <MainScreen places={places} />;
+const isAuthorized = false;
+
+function App({ places }: { places: placeCards }): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact>
+          <MainScreen places={places} />
+        </Route>
+        <Route path='/login' exact>
+          <LoginScreen />
+        </Route>
+        <PrivateRoute
+          path='/favorites'
+          render={() => <FavoritesScreen />}
+          exact
+          isAuthorized={isAuthorized}
+        />
+        <Route path='/offer/:id' exact>
+          <PropertyScreen />
+        </Route>
+        <Route>
+          <Page404 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
